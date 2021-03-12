4c7a57b1d8f29efae7599fcf90e173d0cca3eb93
Deepali Nagwade <nagwade.deepali@gmail.com>
2021-03-12 17:43:57 +0530

    Added comments for understanding

--- src/main/java/com/lzwcompression/lzwcompressionalgorithm/compression/Compression.java
+++ src/main/java/com/lzwcompression/lzwcompressionalgorithm/compression/Compression.java
@@ -17,13 +17,15 @@ import org.springframework.stereotype.Component;
 public class Compression {
 
     public String compressFile(String inputFileName) {
-
-        double MAX_TABLE_SIZE = Math.pow(2, 16);   		//MAX_TABLE_SIZE is based on the input bit length
-        double tableSize = 256; 						//current size of the hashtable
+	
+	//MAX_TABLE_SIZE is based on the input bit length
+        double MAX_TABLE_SIZE = Math.pow(2, 16); 
+	//current size of the hashtable
+        double tableSize = 256; 						
                 
-        Map<String,Integer> map = new HashMap<>(); 				//Data structure
+        Map<String,Integer> map = new HashMap<>(); //Data structure
         for (int i = 0; i < 256; i++) {
-            map.put(Character.toString((char) i), i); 		//Initialize code for individual characters
+            map.put(Character.toString((char) i), i);//Initialize code for individual characters
         }
 
         String outputFileName = inputFileName.substring(0,inputFileName.indexOf(".")) + ".lzw";
@@ -32,49 +34,47 @@ public class Compression {
 		BufferedReader bufferedReader = null;
 		BufferedWriter output = null; 
 		
-		try {
-            
+		try{
 			bufferedReader = new BufferedReader(new FileReader(inputFileName));
-			output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFileName),"UTF-16BE"));  //Encoded data stored in 16 Bit format
-			
-
+			//Encoded data stored in 16 Bit format
+			output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFileName),"UTF-16BE"));
 			//Read the input file and store in a string
-            while((line = bufferedReader.readLine()) != null) {
-            	text = line;
-            }      
+            		while((line = bufferedReader.readLine()) != null) {
+            			text = line;
+            		}      
 
-            String encodeString = ""; 							// Initialize empty string
-            if(text!=null){										//Condition to check for empty input file
+            		String encodeString = ""; // Initialize empty string
+			//Condition to check for empty input file
+            		if(text!=null){
 				for(char c: text.toCharArray()) {  
 					String symbol = "" + c;
 					if (map.containsKey(encodeString + symbol))
 						encodeString = encodeString + symbol;
-					else {
-						output.write(map.get(encodeString));		//Write to file the encoded string
-						if(tableSize < MAX_TABLE_SIZE)				//check if table not full
-							map.put(encodeString + symbol, (int) tableSize++);  //add code to the table and increase tableSize
+					else{
+						//Write to file the encoded string
+						output.write(map.get(encodeString));
+						//check if table not full
+						if(tableSize < MAX_TABLE_SIZE)	
+							//add code to the table and increase tableSize
+							map.put(encodeString + symbol, (int) tableSize++);  
 						encodeString = "" + symbol;
 					}
 				}
-            	output.write(map.get(encodeString));				//Write to file the encoded string
-        	}
-            bufferedReader.close();    //Close the reader
-	        output.flush();				//Flush the buffer
-	        output.close();				//Close the writer
+				//Write to file the encoded string
+            			output.write(map.get(encodeString));				
+        		}
+			
+            	bufferedReader.close();   //Close the reader
+	        output.flush();		  //Flush the buffer
+	        output.close();		  //Close the writer
 
-	        }
-	    catch(FileNotFoundException ex) {
-	        System.out.println("Unable to open file '" + inputFileName + "'");                
-	    }
-	    
-		catch(IOException ex) {
-	            System.out.println("Error reading file '" + inputFileName + "'");                  
-	    }
-
-		catch(InvalidPathException ex){
+	        }catch(FileNotFoundException ex) {
+	        	System.out.println("Unable to open file '" + inputFileName + "'");                
+	    	}catch(IOException ex) {
+	            	System.out.println("Error reading file '" + inputFileName + "'");                  
+		}catch(InvalidPathException ex){
 			System.out.println("bypass");
 			return outputFileName;
-
 		}
 
         return outputFileName;
